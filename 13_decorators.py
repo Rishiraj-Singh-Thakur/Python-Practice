8212ad1824af74d785318821d566e582fa63c358
Rishiraj Singh Thakur <you@example.com>
2026-09-22 14:11:55 +0530

    practice

--- 13_decorators.py
+++ 13_decorators.py
@@ -1,11 +1,42 @@
-def logger(func):
-    def wrapper():
-        print("Function is called")
-        func()
-    return wrapper
+# def logger(func):
+#     def wrapper():
+#         print("Function is called")
+#         func()
+#     return wrapper
 
-@logger
-def say_hello():
-    print("Hello")
+# @logger
+# def say_hello():
+#     print("Hello")
 
-say_hello()
+# say_hello()
+
+# def set_alarm(func):
+#     def exams():
+#         print( "my exams are near so")
+#         func()
+#     return exams
+# @set_alarm
+# def get_up():
+#     print("i want to get up early in the morning")
+
+# get_up()
+
+# use decorator
+def exams(func1 , func2 ,func3):
+    def tt():
+        print("my time table like ",end="")
+        func1()
+        func2()
+        func3()
+    return tt
+
+def f1():
+    print("first exams of english")
+
+def f2():
+    print("second of math")
+
+def f3():
+    print("third of chemistry and physics")
+
+exams(f1,f2,f3)()
