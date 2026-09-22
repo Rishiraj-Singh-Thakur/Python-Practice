3c576510fa440d5263132f93a0de4f0222f34096
Rishiraj Singh Thakur <you@example.com>
2026-09-22 13:19:34 +0530

    practice

--- 12_oop_basics.py
+++ 12_oop_basics.py
@@ -15,16 +15,37 @@
 # print(f"the salary of e2 is {e2.company}")
 
 # now constructor
-class employ():
-    company = "hp"
-    location ="indore"
-    def __init__(self , id , roll , salary):
-        self.id = id
-        self.roll = roll
-        self.salary = salary
-e1 = employ(1 , "hr" , 400000)
-e2 = employ(2 , "jr" , 300000)
-e3 = employ(3 , "sre" , 340000)
-print(e1.__dict__)
-print(e2.__dict__)
-print(e3.__dict__)
+# class employ():
+#     company = "hp"
+#     location ="indore"
+#     def __init__(self , id , roll , salary):
+#         self.id = id
+#         self.roll = roll
+#         self.salary = salary
+# e1 = employ(1 , "hr" , 400000)
+# e2 = employ(2 , "jr" , 300000)
+# e3 = employ(3 , "sre" , 340000)
+# print(e1.__dict__)
+# print(e2.__dict__)
+# print(e3.__dict__)
+
+# Inheritance and polymorphism
+class animals():
+    location  = "india"
+    def __init__(self , name , age):
+        self.age = age
+        self.name = name
+
+    def speak(self):
+        print("animal sound .....")
+
+    def eat(self):
+        print("it is eating")
+
+class dog(animals):       #Inherit
+        def speak(self):
+            print("woof.....!:-)")
+d1 = dog("honny" , 12)
+print(d1.__dict__)
+d1.eat()
+d1.speak()
