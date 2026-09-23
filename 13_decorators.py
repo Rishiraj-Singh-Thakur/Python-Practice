85fd0f71cfafbb649757732450e99645c8c4673e
Rishiraj Singh Thakur <you@example.com>
2026-09-23 13:29:54 +0530

    revetion

--- 13_decorators.py
+++ 13_decorators.py
@@ -22,21 +22,39 @@
 # get_up()
 
 # use decorator
-def exams(func1 , func2 ,func3):
-    def tt():
-        print("my time table like ",end="")
-        func1()
-        func2()
-        func3()
-    return tt
+# def exams(func1 , func2 ,func3):
+#     def tt():
+#         print("my time table like ",end="")
+#         func1()
+#         func2()
+#         func3()
+#     return tt
 
-def f1():
-    print("first exams of english")
+# def f1():
+#     print("first exams of english")
 
-def f2():
-    print("second of math")
+# def f2():
+#     print("second of math")
 
-def f3():
-    print("third of chemistry and physics")
+# def f3():
+#     print("third of chemistry and physics")
 
-exams(f1,f2,f3)()
+# exams(f1,f2,f3)()
+
+# getter and setter
+class car():
+    def __init__(self , make , model):
+        self.__make = make
+        self.__model= model
+
+    def get_make(self):
+        return self.__make
+
+    def get_model(self):
+        return self.__model
+
+c1 = car(1999 , "bmw")
+print(c1.get_make())
+c2 = car(1999 , "m5c")
+print(c2.get_model())
+print(c1.get_model())
