c8983d7b8b958c58252478fc80a0c5a2b0a9c8be
Rishiraj Singh Thakur <you@example.com>
2026-09-24 13:47:10 +0530

    practice of I/O files

--- 15_file_handling.py
+++ 15_file_handling.py
@@ -1,6 +1,24 @@
-with open("sample.txt", "w") as f:
-    f.write("Python practice file")
+# read file
 
+file = open("my.txt" , "r")
+content = file.read()
+print(content)
 
-with open("sample.txt", "r") as f:
-    print(f.read())
+file.close()
+
+# write a content in file using write() , it clean all content of file
+
+# file = open("my.txt" , "w")
+
+# content = "did practice on me"
+
+# file.write(content)
+# file.close()
+
+# Append the file
+
+file = open("my.txt" , "a")
+
+content = "I am a para for practice the append method of python "
+
+file.write(content)
